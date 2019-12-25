44551623fc41e39a87fde3a3d1ce576c4ee14968
Ridha Rahmi <ridha.rahmi13@gmail.com>
2019-12-25 16:32:50 +0100

    update controller

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -6,7 +6,6 @@ namespace Drupal\send_mail\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 
-
 class MailController extends ControllerBase
 {
 
@@ -19,7 +18,7 @@ class MailController extends ControllerBase
 
         $query = \Drupal::entityQuery('webform_submission')
             ->condition('webform_id', 'subscribe');
-        $result  = $query->execute();
+        $result = $query->execute();
         $submission_data = [];
         foreach ($result as $item) {
             $submission = \Drupal\webform\Entity\WebformSubmission::load($item);
@@ -29,18 +28,37 @@ class MailController extends ControllerBase
 
 
         $mailManager = \Drupal::service('plugin.manager.mail');
+        $sitename = \Drupal::config('system.site')->get('name');
         $langcode = \Drupal::currentUser()->getPreferredLangcode();
         $params['context']['subject'] = 'test Subject';
-        $params['context']['message'] = 'test body';
-        $to = 'developpementweb77@gmail.com';
-        $m = $result['result'] = $mailManager->mail('system', 'mail', $to, $langcode, $params);
-       //kint($result['result']);
+        $params['context']['message'] = t('Your wonderful message about @sitename', array('@sitename' => $sitename));
+        $to = 'ridha.rahmi@hotmail.com';
+        $result['result'] = $mailManager->mail('system', 'mail', $to, $langcode, $params);
+        //kint($result['result']);
 
-            return [
-                '#type' => 'markup',
-                '#markup' => $this->t('test Send Newsletter'),
-            ];
+//        $lang = \Drupal::languageManager()->getCurrentLanguage()->getId();
+//        $sitename = \Drupal::config('system.site')->get('name');
+//        $langcode = \Drupal::config('system.site')->get('langcode');
+//
+//
+//        $to = 'ridha.rahmi@hotmail.com';
+//        $reply = NULL;
+//        $send = TRUE;
+//
+//        $params['message'] = t('Your wonderful message about @sitename', array('@sitename' => $sitename));
+//        $params['subject'] = t('Message subject');
+//        $params['options']['username'] = 'ridha rahmi';
+//        $params['options']['title'] = t('Your wonderful title');
+//        $params['options']['footer'] = t('Your wonderful footer');
+//
+//        $mailManager = \Drupal::service('plugin.manager.mail');
+//        $mailManager->mail('system', $to, $langcode, $params, $reply, $send);
 
+
+        return [
+            '#type' => 'markup',
+            '#markup' => $this->t('Your mail has been sent successfuly ! Thank you for your feedback!'),
+        ];
 
 
     }
